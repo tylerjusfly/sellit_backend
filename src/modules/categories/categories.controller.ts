b0cff6b78029ca85a5b365aa675948aa5c2e6765
tylerjusfly <tylerjusfly1@gmail.com>
2024-01-03 22:23:57 +0100

    feat: added  fetch all categories feature

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { verifyToken } from '../../middlewares/verifyauth';
-import { CreateCategories } from './categories.service';
+import { CreateCategories, fetchCategories } from './categories.service';
 
 const categoriesRouter = Router();
 
 categoriesRouter.post('/', /*verifyToken,*/ CreateCategories);
+categoriesRouter.get('/', /*verifyToken,*/ fetchCategories);
 
 export const CategoriesController = { router: categoriesRouter };
--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -5,6 +5,7 @@ import { dataSource } from '../../database/dataSource';
 import { Categories } from '../../database/entites/categories.entity';
 import { Shop } from '../../database/entites/shop.entity';
 import { GenerateCategoryid } from '../../utils/generateIds';
+import { IPaginate } from '../../interfaces/pagination';
 
 const isCategoryUnique = async (shopId: number, name: string): Promise<boolean> => {
 	const existingCategory = await dataSource
@@ -53,6 +54,47 @@ export const CreateCategories = async (req: Request, res: Response) => {
 		const result = await categoryCreate.save();
 
 		return handleSuccess(res, result, 'created category', 201, undefined);
+	} catch (error) {
+		return handleError(res, error);
+	}
+};
+
+export const fetchCategories = async (req: Request, res: Response) => {
+	try {
+		const { shop_id, page, limit }: { shop_id?: number; page?: number; limit?: number } = req.query;
+
+		if (!shop_id) {
+			return handleBadRequest(res, 400, 'shop is required');
+		}
+
+		const page_limit = limit ? Number(limit) : 10;
+
+		const offset = page ? (Number(page) - 1) * page_limit : 0;
+
+		const isShop = await dataSource
+			.getRepository(Shop)
+			.createQueryBuilder('shop')
+			.where('shop.id = :id', { id: shop_id })
+			.getOne();
+
+		if (!isShop) return handleBadRequest(res, 404, 'shop not found');
+
+		const [Category, total] = await Categories.findAndCount({
+			where: {
+				shop_id,
+			},
+			skip: offset,
+			take: page_limit,
+		});
+
+		const paging: IPaginate = {
+			totalItems: total,
+			currentPage: page ? Number(page) : 1,
+			totalpages: Math.ceil(total / page_limit),
+			itemsPerPage: page_limit,
+		};
+
+		return handleSuccess(res, Category, `categories`, 200, paging);
 	} catch (error) {
 		return handleError(res, error);
 	}
